da903edd14a4184510d2b719ae93cdcc1e59e76a
Michael Watkins <watkinsmichaeljames@gmail.com>
2021-09-03 14:39:22 -0500

    final 1 page version

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ const inputs = document.querySelectorAll(".form__input");
 const whatsappLinks = document.querySelectorAll(".whatsapp-item");
 const instagramLink = document.querySelector(".instagram-item");
 const navLinks = document.querySelectorAll(".navigation__link");
+const swiperCards = document.querySelectorAll(".swiper-card");
 
 //SWIPER FUNCTIONALITY. slidesperview checks width > 900 then sets visible slides to 3 or 1
 const swiper = new Swiper(".swiper-container", {
   spaceBetween: 30,
-  slidesPerView: myVw > 900 ? 3 : 1,
+  slidesPerView: myVw > 1300 ? 3 : 1,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -29,5 +30,14 @@ const refresher = function () {
   // iterate through the form inputs and set value to '' in each.
   inputs.forEach((element) => (element.value = ""));
 };
+
+// immediately invoked function to set all testimonials to height of largest testimonial
+(function () {
+  const largestCardHeight = document.querySelector(
+    ".swiper-card-largest"
+  ).offsetHeight;
+
+  swiperCards.forEach((card) => (card.style.height = `${largestCardHeight}px`));
+})();
 
 refresher();
